ca32cc0edad40ceab5a8eea1af2bdc3cb5a121de
unknown <ymzemanate@soy.sena.edu.co>
2023-11-16 09:39:34 -0500

    34

---
+++ concert_page/src/App/App.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+//import { useRoutes, BrowserRoutes } from 'react-router-dom'
+import Navbar from '../components/navbar/index.jsx'
+import Api from '../components/Layout/Card/index.jsx'
+function App() {
+  return (
+    < >
+      <Navbar />
+      <Api/>
+    </>
+   
+  )
+}
+
+export default App
